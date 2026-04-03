62017d091b17940f2cd1403776421cb0ccbc8649
Kentaro Yoshitomi <yositomi@las.osakafu-u.ac.jp>
2026-04-03 17:04:38 +0900

    Add comment to ky_linear_algebra.txt

---
+++ 10txt2mac.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+infile="$1"
+outfile="${infile%.txt}.mac"
+
+perl -0pe '
+  s@/\*.*?\*/@@gs;   # コメント削除
+  s/\s*\z//;         # 末尾の空行削除
+' "$infile" > "$outfile"
+
+echo "出力しました: $outfile"
